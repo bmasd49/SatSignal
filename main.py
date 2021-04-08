eaae5c5fcea75fb4d1353988bccdc7172ceb2b26
bmasd49 <bmasd49@gmail.com>
2021-04-08 22:27:29 +0900

    fixing IOtools

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.io import wavfile
-import scipy.signal
-import os
 import tools.signalProcessing as signal
 import tools.IOtools as io
-import tools.signalTools as tools
 
 if __name__=='__main__':
     APT = signal.signal('NOAA15', './inputSignals/station1_yagi_SDRSharp_20170312_060959Z_137650kHz_IQ.wav', signalOffset=137.65e6, timeStep=1.)
--- tools/IOtools.py
+++ tools/IOtools.py
@@ -15,21 +15,23 @@ def plotting(signal, steps = None):
     scale = 1e-3 #convert Hz to kHz
     f = signal.fullFreq * scale
     for i, step in enumerate(steps):
+        print(f"Step {step}/{len(steps)}")
         _, mag, centroid = signal.spectrogram(step=step)
         # f *= scale
         centroid *= scale
         plt.plot(f, mag, '.', markersize=1.)
-        plt.axvline(x=centroid, color='red', markersize=0.1)
+        plt.axvline(x=centroid, color='red', markersize=0.1, label=f'Centroid={centroid:.1f}kHz')
         plt.ylim([-50,100])
         plt.yticks(np.arange(100,-50,-10))
         plt.xticks(np.arange(-signal.bandWidth/2, signal.bandWidth/2+10e3, 10e3)*scale)
-        plt.title(f'Spectrogram at step {step:02d} with each step = {signal.timeStep}s')
-        plt.xlabel('Frequency (kHz)')
+        plt.title(f'Frequency domain at step {step:02d} with each step = {signal.timeStep}s')
+        plt.xlabel(f'Frequency (kHz), offset={int(signal.signalOffset/1000)}kHz')
         plt.ylabel('Amplitude (dB)')
+        plt.legend()
         plt.grid()
         plt.savefig(f'./gif/{i:03d}.png')
         plt.clf()
-    os.system("ffmpeg -y -i ./gif/%03d.png -i ./gif/palette.png -lavfi paletteuse -r 5 output.gif")
+    os.system("ffmpeg -y -framerate 5 -i ./gif/%03d.png -i ./gif/palette.png -lavfi paletteuse -r 5 output.gif")
     # for i, time in enumerate(t[1:-1]):
         # plt.plot(f, spectrum[:,i+1], '.', markersize=1)
         # plt.axvline(x=maxFreq[i+1])
